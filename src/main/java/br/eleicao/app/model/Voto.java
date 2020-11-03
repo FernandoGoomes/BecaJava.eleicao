616f8543d5818839230795e072d7e3fda8409be2
Fernando Marsicano Gomes <fmarsica@everis.com>
2020-11-03 14:59:44 -0300

    first commit

---
+++ src/main/java/br/eleicao/app/model/Voto.java
@@ -0,0 +1,54 @@
+package br.eleicao.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Voto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "EleitorId")
+	private Eleitor Eleitor;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "CandidatoId")
+	private Candidato Candidato;
+	
+		
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public Eleitor getEleitor() {
+		return Eleitor;
+	}
+
+	public void setEleitor(Eleitor eleitor) {
+		Eleitor = eleitor;
+	}
+
+	public Candidato getCandidato() {
+		return Candidato;
+	}
+
+	public void setCandidato(Candidato candidato) {
+		Candidato = candidato;
+	}
+	
+	
+	
+
+}
